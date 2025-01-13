39f873dbfee3d8161c10e6ae4095efcf86c1dcb0
Nick <36767444+njcos@users.noreply.github.com>
2025-01-12 22:26:24 -0500

    Test 4 done.  Exercise 03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -16,5 +16,6 @@ const reverseString = function(string) {
 reverseString("hello")
 reverseString("hello there")
 reverseString('123! abc! Hello, Odinite.')
+reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
