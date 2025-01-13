a04efe17bc44c9edebcd12c9bd030c73eae2114e
Nick <36767444+njcos@users.noreply.github.com>
2025-01-12 22:22:27 -0500

    Test 1 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = [];
+    let reverseArray = [];
+    let newString = '';
 
+    for (char of string) {
+        array.push(char)
+    }
+    reverseArray = array.reverse();
+    
+    for (char of array) {
+        newString +=char
+    }
+    return newString;
 };
-
+reverseString("hey")
 // Do not edit below this line
 module.exports = reverseString;
