a214a909232cc1781e3829c6437997868395b953
Nick <36767444+njcos@users.noreply.github.com>
2025-01-12 21:53:11 -0500

    Test 5 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,21 @@
 const repeatString = function(string, num) {
     let hey = "";
+    if (num >= 0) {
+
     for (let i = 0; i < num; i++) {
         hey += string 
     }
+    } else {
+        hey = "ERROR"
+    } 
     return hey;
+
 };
 
 repeatString("hey", 3)
 repeatString('hello', 10)
 repeatString('hi', 1)
 repeatString('bye', 0)
-
+repeatString('goodbye', -1)
 // Do not edit below this line
 module.exports = repeatString;
