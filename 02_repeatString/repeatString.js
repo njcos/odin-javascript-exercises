4cbdfd12639b05136006ad413f5a6279566dc83a
Nick <36767444+njcos@users.noreply.github.com>
2025-01-12 21:43:47 -0500

    Test 1 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let hey = "";
+    for (let i = 0; i < num; i++) {
+        hey += string 
+    }
+    return hey;
 
 };
 
+repeatString("hey", 3)
 // Do not edit below this line
 module.exports = repeatString;
