5ef787143eadc684b5447ee5ac3cb665dc6081ed
Nick <36767444+njcos@users.noreply.github.com>
2025-01-13 22:12:32 -0500

    07 exercises complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+    let convert = ((far - 32)*5/9)
+    let rounded = parseFloat(convert.toFixed(1))
+    console.log(rounded)
+    return rounded
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let convert = (cel * 9/5 + 32)
+  let rounded = parseFloat(convert.toFixed(1))
+      console.log(rounded);
+      return rounded
+      
 };
 
+
+convertToCelsius(32)
+convertToFahrenheit(0)
+convertToCelsius(100)
+convertToFahrenheit(73.2)
+convertToCelsius(-100)
+convertToFahrenheit(-10)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
