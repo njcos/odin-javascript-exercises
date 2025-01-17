9c10132e30c13c077a60e9ba8c884744f03ab7ba
Nick <36767444+njcos@users.noreply.github.com>
2025-01-16 23:22:08 -0500

    Completed 09, not as efficient as I could.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let word = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    const seperated = word.split('')
+    const reversed = seperated.reverse();
+    const joined = reversed.join('')
+    console.log(word)
+    console.log(joined)
+    if(word === joined) {
+        return true
+    } else {
+        return false
+    }
 };
+
+palindromes('race car')
 
 // Do not edit below this line
 module.exports = palindromes;
