a93e82aec7364a82283a75750777fbcaeed54247
Nick <36767444+njcos@users.noreply.github.com>
2025-01-16 23:54:43 -0500

    exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let n1 = 0;
+    let n2 = 1;
+    let next; 
 
+    if(parseInt(number) >= 0){
+        for(let i = 1; i <= number; i++) {
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next   
+        }
+        return n1
+    } else {
+        return "OOPS"
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
