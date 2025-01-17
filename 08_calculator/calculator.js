c10b313d28fa364a3d15d1497628942b77cd7718
Nick <36767444+njcos@users.noreply.github.com>
2025-01-16 22:49:34 -0500

    08 exercises completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
+const add = function(a, b) {
+	return a+b;
+};
+
+const subtract = function(a, b) {
+  return a-b
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+
+   const original = 0
+   const total = array.reduce((a, b) => a + b, original)
+   return total
+
+};
+
+
+const multiply = function(array) {
+  const original = 1
+  const total = array.reduce((a,b) => a*b, original)
+  return total
+};
+
+
+const power = function(a, b) {
+  return Math.pow(a,b)
 	
 };
 
-const sum = function() {
+const factorial = function(num) {
+  if(num === 0 || num === 1) {
+    return 1
+  }
+
+  for(let i = num - 1; i >= 1; i--){
+    num = num * i
+  }
+  console.log(num)
+  return num
+
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+factorial(5)
 
 // Do not edit below this line
 module.exports = {
