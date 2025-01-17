1a365f84c8e196e9ff842f3cee6973746dcaba57
Nick <36767444+njcos@users.noreply.github.com>
2025-01-17 00:01:05 -0500

    exercise 11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+const getTheTitles = function(books) {
+    const titles = books.map(item => item.title)
+
+    console.log(titles)
+    return titles
 
 };
 
+
+getTheTitles(books)
 // Do not edit below this line
 module.exports = getTheTitles;
